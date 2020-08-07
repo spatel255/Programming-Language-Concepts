69034fe3a17089840e6b1aca11aab7345442b692
Shilp Patel <47124685+spatel255@users.noreply.github.com>
2020-08-07 15:54:31 -0400

    Update wae.scala

--- wae.scala
+++ wae.scala
@@ -5,136 +5,60 @@ import scalajs.js
 @js.annotation.JSExportTopLevel("ScalaFiddle")
 object WAE {
 
-abstract class Node
+  abstract class Node
 
-case class WAENode(
-  nodeType:
-  String, // will be "number", "id", "+", "-", "*", "/", "if", or "with"
-  numberValue: Double = 0.0, // used to store number value
-  idValue: String = "", // used to store ia value
-  child1: WAENode = null, // used for if, with, +, -, *, /
-  child2: WAENode = null, // used for if, +, -, *, /
-  child3: WAENode = null, // used for if
-  vars: List[(String, WAENode)] = List[(String, WAENode)]() // used for with
+  case class WAENode(
+    nodeType: String,
+    numValue: Double = 0.0,
+    idValue: String = "",
+    child1: WAENode = null,
+    child2: WAENode = null,
+    child3: WAENode = null,
+    vars: List[(String,WAENode)] = List[(String,WAENode)]()
   ) extends Node
 
-  def eval(s: WAENode, a: Map[String, Double]) : Double =
-  {
+  def eval(e: WAENode, d: Map[String,Double]): Double = {
+    if (e.nodeType == "num") e.numValue
+    else if(e.nodeType == "id") {
 
-    if (
-      s.nodeType == "id"
-      )
-      return
-      (
-        a get s.idValue
-        )
-        .getOrElse(0)
-    if (
-      s.nodeType == "number"
-      )
-      return s.numberValue
-    else throw new Exception("Error!")
-      if (
-        s.nodType == "+" && s.child1.nodeType == "number" && s.child2.nodeType == "number"
-        )
-        {
-          return s.child1.numberValue + s.child2.numberValue
+      if(d.exists(x => x._1 == e.idValue)) d(e.idValue)
+      else throw new Exception("Uninstantiated variable error")
     }
-    if (
-      s.nodeType == "-" && s.child1.nodeType == "number" && s.child2.nodeType == "number"
-      )
-      {
-      if (
-        s.child2.numberValue != 0
-        )
-        {
-        return
-        s.child1.numberValue - s.child2.numberValue
-      }
-      else
-      {
-        throw new Exception("Division Error!")
-      }
-    }
-    if (
-      s.nodeType == "*" && s.child1.nodeType == "number" && s.child2.nodeType == "number"
-      )
-      {
-      return
-      s.child1.numberValue * s.child2.numberValue
-    }
-    if (
-      s.nodeType == "/" && s.child1.nodeType == "number" && s.child2.nodeType == "number"
-      )
-      {
-      return
-      s.child1.numberValue / s.child2.numberValue
-    }
-    if (
-      s.child1.nodeType == "+" || s.child1.nodeType == "-" || s.child1.nodeType == "*" || s.child1.nodeType == "/"
-      )
-      {
-      return
-      eval(
-        s.child1, a
-        )
-    }
-    if (
-      s.child2.nodeType == "+" || s.child2.nodeType == "-" || s.child2.nodeType == "*" || s.child2.nodeType == "/"
-      )
-      {
-      return
-      eval(
-        s.child2, a
-        )
-    }
-    if (
-      s.child3.nodeType == "+" || s.child3.nodeType == "-" || s.child3.nodeType == "*" || s.child3.nodeType == "/"
-      )
-      {
-      return
-      eval(
-        s.child3, a
-        )
-    }
-    else
+    else if(e.nodeType == "+")
     {
-      return 0
+      eval(e.child1,d)+eval(e.child2,d)
     }
-    if (
-      s.nodeType == "if" && s.child1.nodeType == "number" && s.child2.nodeType == "number" && s.child3.nodeType == "number"
-      )
-      {
-      return
-      s.child1.numberValue + s.child2.numberValue + s.child3.numberValue
+    else if(e.nodeType == "-")
+    {
+      eval(e.child1,d)-eval(e.child2,d)
     }
-    if (
-      s.nodeType == "with"
-      )
-      {
-      a += [s.vars.String, s.vars.WAENods.numberValue]
+    else if(e.nodeType == "*")
+    {
+      eval(e.child1,d)*eval(e.child2,d)
+    }
+    else if(e.nodeType == "/")
+    {
+      if(eval(e.child2,d)!=0) eval(e.child1,d)/eval(e.child2,d)
+      else throw new Exception("Divide by zero error")
+    }
+    else if(e.nodeType == "if")
+    {
+      if (eval(e.child1,d)!=0) eval(e.child2,d)
+      else eval(e.child3,d)
+    }
+    else if(e.nodeType == "with")
+    {
+      //populate dictionary
+      var a = d
+      for(l<-e.vars)
+        {
+          a += (l._1 -> eval(l._2, d))
+        }
+      //println(a)
+      eval(e.child1, a)
+    }
 
-    if (s.child1 != null)
-        {
-        return
-        eval(
-          s.child1, a
-          )
-      }
-    if (s.child2 != null)
-        {
-        return
-        eval(
-          s.child2, a
-          )
-      }
-    if (s.child3 != null)
-        {
-        return
-        eval(
-          s.child3, a
-          )
-      }
-    }
+    else throw new Exception("Illegal Syntax")
   }
+
 }
